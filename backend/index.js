5f32ec382ab3d4056692c29a14a96a6e847fe016
Taher Najim <taher.najim@yahoo.com>
2024-02-06 19:27:36 +0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send("Hello World")
+})
+
+app.listen(3000, () => {
+  console.log("Listening to localhost 3000...");
+})
