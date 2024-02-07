c2df7de5330a404635c17ec77b33ea6aac016f4a
Taher Najim <taher.najim@yahoo.com>
2024-02-07 18:56:12 +0800

    connected to mysql server, successfuly fetched data

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "sonics",
+  database: "test"
+});
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   res.send("Hello World")
+})
+
+app.get("/books", function (req, res) {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  })
 })
 
 app.listen(3000, () => {
